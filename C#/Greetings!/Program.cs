a1853a8a71af42953673cba102d61c5e8e78ab28
Sebastian Angin <zeb_angin@msn.com>
2021-03-27 14:51:06 +0100

    Greetings solution

---
+++ C#/Greetings!/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+
+namespace Greetings_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+             while((line = Console.ReadLine()) != null)
+            {
+                String[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+                Start(split);
+               
+            }
+        }
+
+        static void Start(String[] split)
+        {
+            for (var i = 0; i < split.Length; i++)
+            {
+                var builder = new StringBuilder();
+                foreach (char item in split[i])
+                {
+                    builder.Append(item);
+                    if (item.Equals('e'))
+                    {
+                        builder.Append("e");
+                    }
+                  
+                }
+                String txt = split[i];
+                txt = builder.ToString();
+                Console.WriteLine(txt);
+            }
+            
+        }
+    }
+}
