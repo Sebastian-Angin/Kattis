9fca45127544d503c82e5374f7a1b487480ebee7
Sebastian-Angin <81474047+Sebastian-Angin@users.noreply.github.com>
2022-09-23 13:57:31 +0200

    Solved FYI for c#

---
+++ C#/Easy/FIY/FIY.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace FIY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            string input = "No input";
+            string output = "0";
+
+            /*
+            string inputFile = @"C:\Users\zeb_a\Documents\Kattis\C#\Easy\FIY\fyi\a002.in";
+
+            input = File.ReadAllText(inputFile);
+            */
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                // string[] split = input.Split(new char[] {' '}, StringSplitOptions.None);
+                input = line;
+                string firstThree = input.Substring(0, 3);
+                
+
+                if(firstThree == "555")
+                {
+                    output = "1";
+                }
+                else
+                {
+                    output = "0";
+                }
+
+                //output = input;  
+                Console.WriteLine(output);
+            }
+
+
+
+            
+
+            
+            
+        }
+    }
+}
---
+++ C#/README.txt
@@ -0,0 +1,15 @@
+To create a new project use: dotnet new consol
+
+To run program use: dotnet run
+
+
+To read files from Kattis:
+string line;
+while ((line = Console.ReadLine()) != null)
+            {put the entire code here}
+
+To read text from files:
+
+string inputFile = @"Path to file";
+
+input = File.ReadAllText(inputFile);
