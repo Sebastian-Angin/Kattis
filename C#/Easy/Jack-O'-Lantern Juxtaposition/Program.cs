8b582e3394cbfae2c61228e7c13b3f6104ea7c65
Sebastian-Angin <81474047+Sebastian-Angin@users.noreply.github.com>
2022-10-14 20:22:27 +0200

    C# compleated

---
+++ C#/Easy/Jack-O'-Lantern Juxtaposition/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Jack_O__Lantern_Juxtaposition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            long eyes, nose, mouths;
+            while((line = Console.ReadLine()) != null)
+            {
+                string[] split = line.Split(new char[] {' '}, StringSplitOptions.None);
+                eyes = Int64.Parse(split[0]);
+                nose = Int64.Parse(split[1]);
+                mouths = Int64.Parse(split[2]);
+
+                long result = eyes * nose * mouths;
+
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
